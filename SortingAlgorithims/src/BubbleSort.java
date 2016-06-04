01e28c5a8f6e357a6bf765141bfb572f4a3cc420
Jasmine Hurst <jasmine.ehurst@gmail.com>
2016-06-03 21:35:53 -0500

    Initial commit

---
+++ SortingAlgorithims/src/BubbleSort.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+
+public class BubbleSort {
+
+	public void sort(int[] array){
+		int temp;
+		for(int i = 0;i < array.length-1; i++){
+			for(int j = 1; j < array.length-i; j++){
+				if(array[j - 1] > array [j]){
+					temp = array[j - 1];
+					array[j - 1] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(array));
+	}
+}
---
+++ SortingAlgorithims/src/TestAlgorithims.java
@@ -0,0 +1,11 @@
+
+public class TestAlgorithims {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[] = {3,8,1,9,4,5,3};
+		BubbleSort bubbleSort = new BubbleSort();
+		bubbleSort.sort(array);
+	}
+
+}
